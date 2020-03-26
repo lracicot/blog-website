b52abcace4502e6302cc5d602337368fc4a334b2
Louis Racicot <louis.racicot@alayacare.com>
2020-03-26 11:45:35 -0400

    remove express compression

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const compression = require('compression');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const awsServerlessExpress = require('aws-serverless-express')
 const app = require('./app')
 const binaryMimeTypes = [
+  'application/javascript',
+  'application/json',
   'application/octet-stream',
-  'font/eot',  'font/opentype',
+  'application/xml',
+  'font/eot',
+  'font/opentype',
   'font/otf',
   'image/jpeg',
   'image/png',
-  'image/svg+xml'
-];
+  'image/svg+xml',
+  'text/comma-separated-values',
+  'text/css',
+  'text/html',
+  'text/javascript',
+  'text/plain',
+  'text/text',
+  'text/xml'
+]
 
 const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes)
 
