5dd21a7c5c2747dc2781198412787bd57be8ef6a
Louis Racicot <louis.racicot@alayacare.com>
2020-03-26 11:07:51 -0400

    add express compression

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const next = require('next')
+const express = require('express');
+const compression = require('compression');
+const next = require('next');
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 // app.prepare()
 // .then(() => {
-  const server = express()
+const server = express();
+server.use(compression);
 
-  server.get('*', (req, res) => {
-    return handle(req, res)
-  })
+server.get('*', (req, res) => {
+  return handle(req, res);
+});
 
 module.exports = server;
 // })
