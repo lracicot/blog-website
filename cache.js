6b6b633b8578b2c3d68f71f5118312adce7b6227
Louis Racicot <louis.racicot@alayacare.com>
2020-08-14 03:02:51 -0400

    stream for invalidate cache on new or updated posts

---
+++ cache.js
@@ -0,0 +1,56 @@
+const AWS = require("aws-sdk");
+
+const cloudfront = new AWS.CloudFront();
+
+class CacheController {
+  constructor(distributionId) {
+    this.distributionId = distributionId;
+  }
+
+  invalidateUrl(urls) {
+    return new Promise((resolve, reject) => {
+      cloudfront.createInvalidation(
+        {
+          DistributionId: this.distributionId,
+          InvalidationBatch: {
+            CallerReference: Date.now(),
+            Paths: {
+              Quantity: urls.length,
+              Items: urls
+            }
+          }
+        },
+        (err, data) => (err ? reject(err) : resolve(data))
+      );
+    });
+  }
+}
+
+module.exports = (cacheController => async event => {
+  const newPostsUrl = event.Records.filter(
+    record =>
+      record.eventName === "INSERT" && record.eventSource === "aws:dynamodb"
+  ).map(record => record.dynamodb.NewImage.slug.S);
+
+  if (newPostsUrl.length) {
+    await cacheController.invalidateUrl(["/", ...newPostsUrl]);
+  }
+
+  const updatedPostsUrl = event.Records.filter(
+    record =>
+      record.eventName === "MODIFY" && record.eventSource === "aws:dynamodb"
+  ).map(record => record.dynamodb.NewImage.slug.S);
+
+  if (updatedPostsUrl.length) {
+    await cacheController.invalidateUrl([updatedPostsUrl]);
+  }
+
+  const removedPostsUrl = event.Records.filter(
+    record =>
+      record.eventName === "REMOVE" && record.eventSource === "aws:dynamodb"
+  ).map(record => record.dynamodb.NewImage.slug.S);
+
+  if (removedPostsUrl.length) {
+    await cacheController.invalidateUrl(["/", ...removedPostsUrl]);
+  }
+})(new CacheController(process.env.CF_DISTRIBUTION_ID));
